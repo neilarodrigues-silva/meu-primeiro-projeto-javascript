e308b0f71b9a9529b7459e17f6065faf48088757
Neila Rodrigues <neila.silva@gft.com>
2022-12-14 10:44:06 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,9 @@
+//tipo Number
+
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica)
